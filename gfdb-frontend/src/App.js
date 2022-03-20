2bd3d53314494eae9d3430958430360719251ed2
gfdb <francodumoulin123@gmail.com>
2022-03-20 03:43:57 -0400

    added Nav component

--- gfdb-frontend/src/App.js
+++ gfdb-frontend/src/App.js
@@ -1,5 +1,6 @@
 // import './App.css';
 import Home from './components/home/'
+import Nav from './components/nav'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Home />
+        <Nav />
       </header>
     </div>
   );
